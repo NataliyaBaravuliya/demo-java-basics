8bb68f8b408621b3770f36c68e394924344cecb1
NataliyaBaravuliya <NataliyaBaravuliya@gmail.com>
2023-02-27 15:40:33 +0300

    add classMain

---
+++ by/itacademy/nataliyabaravuliya/javabasics/Main.java
@@ -0,0 +1,12 @@
+package by.itacademy.nataliyabaravuliya.javabasics;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your name!");
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name + "!");
+    }
+}
